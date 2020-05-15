20105bb7dbf856482471dba87134c846b136e212
Feng Chun <springfeng@gmail.com>
2020-05-15 19:22:03 +0800

    add more functions

--- src/mG_p4.py
+++ src/mG_p4.py
@@ -1,7 +1,47 @@
 import random
 
+# collection of decimal questions
+
+picker = ('decimalAdd', 'decimalMinus', 'decimalMultiply', 'decimalDivide')
+
+
+def pick():
+    p = random.randint(0, 3)
+    globals()[picker[p]]()
+
+
 def decimalAdd():
     n1 = round(random.random() * 101, 3)
     n2 = round(random.random() * 101, 2)
     ans = input(f'{n1} + {n2} = ')
     print(float(ans) == n1 + n2)
+    print(n1+n2)
+
+
+def decimalMinus():
+    n1 = round(random.random() * 101, 3)
+    n2 = round(random.random() * 101, 2)
+    if n1 < n2:
+        tmp = n1
+        n1 = n2
+        n2 = tmp
+    ans = input(f'{n1} - {n2} = ')
+    print(float(ans) == n1 - n2)
+    print(n1-n2)
+
+
+def decimalMultiply():
+    n1 = round(random.random() * 101, 3)
+    n2 = round(random.random() * 101, 0)
+    ans = input(f'{n1} * {n2} = ')
+    print(float(ans) == n1 * n2)
+    print(n1*n2)
+
+
+def decimalDivide():
+    n1 = round(random.random() * 101, 3)
+    n2 = random.randint(2, 12)
+    n3 = n1 * n2
+    ans = input(f'{n3} / {n2} = ')
+    print(float(ans) == n1)
+    print(n1)
--- src/mQ.py
+++ src/mQ.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from mView import mView
 from ctypes import windll
+import mG_p4
 
 
 class MyApplication(tk.Tk):
@@ -16,8 +17,18 @@ class MyApplication(tk.Tk):
         mv = mView(self)
         mv.grid(sticky=(tk.E + tk.W + tk.N + tk.S))
         self.columnconfigure(0, weight=1)
+        mv.grid(2, 2)
+
+    def generate_timer(self, initial_timer_seconds: int):
+        hh = initial_timer_seconds / 3600
+        mm = initial_timer_seconds / 60 - hh
+        ss = initial_timer_seconds % 3600 % 60
+
+        return f'{str(hh).zfill(2)}:{str(mm).zfill(2)}:{str(ss).zfill(2)}'
 
 
 if __name__ == '__main__':
+    while True:
+        mG_p4.pick()
     app = MyApplication()
     app.mainloop()
